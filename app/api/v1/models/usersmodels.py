eea73c35df4f659190950108be23c8dccca7dd55
philipotieno <tmitchellb007@gmail.com>
2019-01-08 21:55:10 +0300

    [Feature 163046495] Adds registration date

--- app/api/v1/models/usersmodels.py
+++ app/api/v1/models/usersmodels.py
@@ -1,21 +1,23 @@
+import datetime
+
 class User():
 	"""Adds user class"""
 
 	def __init__(self):
 		self.users = {}
 
-	def register_user(self, fname, lname, username, email, password):
+	def register_user(self, firstname, lastname, username, email, phone, registered, password):
 		user_id = str(len(self.users) + 1)
 		reg_user = {
 			"id" : user_id,
-			"firstname" : fname,
-			"lastname" : lname,
+			"firstname" : firstname,
+			"lastname" : lastname,
 			"username" : username,
 			"email" : email,
 			"phone" : phone,
 			"password" : password,
-			"registered" : registered,
-			"admin": False
+			"registered" : datetime.datetime.now()
+			# "admin": False
 		}
 
 		self.users[username] = reg_user
