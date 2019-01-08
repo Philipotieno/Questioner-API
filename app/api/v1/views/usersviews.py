af5fb69151ffc4451f2ed67562148eb5b46e54d3
philipotieno <tmitchellb007@gmail.com>
2019-01-08 17:30:08 +0300

    [Feature 163046495] Adds users view class

--- app/api/v1/views/usersviews.py
+++ app/api/v1/views/usersviews.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
-from app.api.v1.models.usermodels import User
+from app.api.v1.models.usersmodels import User
 import os
 
 v1_user = Blueprint('users', __name__)
 
-user_inst = User() #user class instance
+user_inst = User() #meetup class instance
 
 
 @v1_user.route('/register', methods=['POST'])
