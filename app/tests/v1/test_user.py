43c6f7d2c75eada9321ae633a7dab16b67772e6f
philipotieno <tmitchellb007@gmail.com>
2019-01-09 00:07:18 +0300

    [Start a63046673] Adds failing tests to test user log in

--- app/tests/v1/test_user.py
+++ app/tests/v1/test_user.py
@@ -19,6 +19,20 @@ class TestUser(TestSetup):
         self.assertIn('User Registered successfully!', msg['message'])
         self.assertTrue(res.content_type == 'application/json')
 
+    def test_registered_user_login(self):
+        self.client.post(
+            'auth/register',
+            data=json.dumps(self.user_1),
+            content_type='application/json')
+
+        res = self.client.post(
+            'auth/login',
+            data=json.dumps(self.user_1),
+            content_type='application/json')
+        msg = json.loads(res.data.decode('UTF-8'))
+        self.assertIn('sucessfull login', msg['message'])
+        self.assertTrue(res.content_type == 'application/json')
+        self.assertEqual(res.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
