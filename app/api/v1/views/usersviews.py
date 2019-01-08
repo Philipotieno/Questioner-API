f7eb9b1212a6a51f5745f0f0e2194d3d24be870d
philipotieno <tmitchellb007@gmail.com>
2019-01-08 21:24:35 +0300

    [Feature 163046495] Adds endpoint for user details

--- app/api/v1/views/usersviews.py
+++ app/api/v1/views/usersviews.py
@@ -1,12 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 from app.api.v1.models.usersmodels import User
 import os
 
 v1_user = Blueprint('users', __name__)
 
-user_inst = User() #meetup class instance
+user_inst = User() #user class instance
 
 
-@v1_user.route('/register', methods=['POST'])
+@v1_user.route('/', methods=['POST'])
 def registered_user():
-	pass
+	data = request.get_json()
+	firstname = data["firstname"]
+	lastname = data["lastname"]
+	username = data["username"]
+	email = data["email"]
+	phone = data["phone"]
+	password = data["password"]
+
+	return jsonify({'message': 'User Registered successfully!'}), 201
