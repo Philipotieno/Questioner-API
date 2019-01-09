d46d6be191a20b99596a53840159ca5d9510f8f4
philipotieno <tmitchellb007@gmail.com>
2019-01-09 09:17:57 +0300

    [Feature 163037431] Registers meetup blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from instance.config import app_config
 from app.api.v1.views.usersviews import v1_user
+from app.api.v1.views.meetupviews import v1_meetups
 
 def create_app(env_name):
 	""" Cretae app """
@@ -11,6 +12,7 @@ def create_app(env_name):
 	app = Flask(__name__)
 	app.config.from_object(app_config[env_name])
 	app.register_blueprint(v1_user, url_prefix='/v1')
+	app.register_blueprint(v1_meetups, url_prefix='/v1/meetups')
 
 
 	return app
