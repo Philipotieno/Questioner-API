a9d85da5e79c89a49e5b3d03e0be0fa393aee560
philipotieno <tmitchellb007@gmail.com>
2019-01-12 18:42:51 +0300

    [Chore 163156644] Tests for posted question

---
+++ app/tests/v1/test_questions.py
@@ -0,0 +1,21 @@
+import unittest
+import json
+
+from app.tests.v1.base import TestSetup
+
+class TestQuestion(TestSetup):
+
+    def test_asked_question(self):
+        """ Test new meetup can be created """
+        res = self.client.post(
+            '/api/v1/questions',
+            data=json.dumps(self.question_2),
+            content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+        msg = json.loads(res.data.decode("UTF-8"))
+        self.assertIn("Question created successfully", msg["Message"])
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
