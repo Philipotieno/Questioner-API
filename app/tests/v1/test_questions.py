42777d27536d6c42c2ca55fd8f1c05a7c01c4ce4
philipotieno <tmitchellb007@gmail.com>
2019-01-13 09:08:40 +0300

    [Chore 163156644] Tests for downvoted question

--- app/tests/v1/test_questions.py
+++ app/tests/v1/test_questions.py
@@ -30,6 +30,20 @@ class TestQuestion(TestSetup):
         msg = json.loads(res.data.decode("UTF-8"))
         self.assertIn("you have upvoted a question", msg["message"])
 
+    def test_downvote_a_question(self):
+        """ Test upvote a question """
+        self.client.post(
+            '/api/v1/questions',
+            data=json.dumps(self.question_2),
+            content_type='application/json')
+
+        res = self.client.put(
+            '/api/v1/questions/1/downvote',
+            data=json.dumps(self.downvote),
+            content_type='application/json')
+        self.assertEqual(res.status_code, 200)
+        msg = json.loads(res.data.decode("UTF-8"))
+        self.assertIn("you have downvoted a question", msg["message"])
 
 
 if __name__ == '__main__':
