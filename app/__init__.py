70c1b61b6f6c480ddd27e2a6f09c09c61f24c1a6
philipotieno <tmitchellb007@gmail.com>
2019-01-17 01:52:02 +0300

    [Feature 163223218] Registers blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify
 
 from instance.config import app_config
+from app.api.v1.views.usersviews import v1_user
 from app.api.v2.views.usersviews import v2_user
+from app.api.v1.views.meetupviews import v1_meetups
 from app.api.v2.views.meetupviews import v2_meetups
+from app.api.v1.views.questionsviews import v1_questions
 from app.api.v2.views.questionsviews import v2_questions
 
 def create_app(env_name):
@@ -12,8 +15,11 @@ def create_app(env_name):
 
 	app = Flask(__name__)
 	app.config.from_object(app_config[env_name])
-	app.register_blueprint(v2_user, url_prefix='/api/v1/auth')
-	app.register_blueprint(v2_meetups, url_prefix='/api/v1/meetups')
+	app.register_blueprint(v1_user, url_prefix='/api/v1/auth')
+	app.register_blueprint(v2_user, url_prefix='/api/v2/auth')
+	app.register_blueprint(v1_meetups, url_prefix='/api/v1/meetups')
+	app.register_blueprint(v2_meetups, url_prefix='/api/v2/meetups')
+	app.register_blueprint(v1_questions, url_prefix='/api/v1/questions')
 	app.register_blueprint(v2_questions, url_prefix='/api/v1/questions')
 
 	@app.route('/')
