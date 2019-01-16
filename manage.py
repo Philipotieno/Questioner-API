77f97ebaee37920b4d5bb8d8441af75073c2209f
philipotieno <tmitchellb007@gmail.com>
2019-01-16 15:23:19 +0300

    [Chore 163246860] Adds manage.py

---
+++ manage.py
@@ -0,0 +1,21 @@
+import os
+from flask_script import Manager #class for handling a set of commands
+
+from app.api.v2.models.db import connect_to_db
+from app import create_app
+env_name = os.getenv('FLASK_ENV')
+
+app = create_app(env_name)
+
+manager = Manager(app)
+
+@manager.command
+def create_tables():
+    db.create_tables()
+
+@manager.command
+def drop_tables():
+    db.drop_tables()
+
+if __name__ == '__main__':
+    manager.run() 
