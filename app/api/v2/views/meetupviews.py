f92ce4f7b465afe49abac8e6ad1cd11b0e1ec59e
philipotieno <tmitchellb007@gmail.com>
2019-01-19 18:23:58 +0300

    [Feature 163225667] adds validation

--- app/api/v2/views/meetupviews.py
+++ app/api/v2/views/meetupviews.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify, request
 import datetime
 
 from app.api.v2.models.meetupsmodel import Meetup
+from app.api.v2.views.validator import validate_meetup
 
 from app.api.v2.models.db import Database
 
@@ -12,7 +13,12 @@ v2_meetups = Blueprint('v2_meetups', __name__)
 @v2_meetups.route('', methods=['POST'])
 def create_meetup():
     data = request.get_json()
+    if validate_meetup(data):
+    	return validate_meetup(data)
 
+    if not data or not data["topic"] or not data["location"] or not data["happening_on"] or not data["tags"]:
+        return jsonify({'message': 'All fields are required!'}), 400
+    
     meetup_details = Meetup(
         data['topic'],
         data['location'],
