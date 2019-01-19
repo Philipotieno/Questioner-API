517e736a0ee04bbd096fc0242f02a4661fdbbb97
philipotieno <tmitchellb007@gmail.com>
2019-01-19 17:02:16 +0300

    [Feature 163225667] Adds route to view meetups

--- app/api/v2/views/meetupviews.py
+++ app/api/v2/views/meetupviews.py
@@ -1,6 +1,6 @@
-import os
 import psycopg2
 from flask import Blueprint, jsonify, request
+import datetime
 
 from app.api.v2.models.meetupsmodel import Meetup
 
@@ -8,12 +8,20 @@ from app.api.v2.models.db import Database
 
 v2_meetups = Blueprint('v2_meetups', __name__)
 
-db = Database()
-cur = db.conn.cursor()
 
 @v2_meetups.route('', methods=['POST'])
 def create_meetup():
-    pass
+    data = request.get_json()
+
+    meetup_details = Meetup(
+        data['topic'],
+        data['location'],
+        data['tags'],
+        data['happening_on']
+    )
+    if meetup_details.create_meetup():
+    	return jsonify({'message': 'Meetup created!'}), 201
+    return jsonify({'message': 'Topic {} already exists choose another topic'.format(data['topic'])}), 409
 
 @v2_meetups.route('/upcoming', methods=['GET'])
 def get_meetups():
