2b46eda8c9d22f5c4cbd7b35a12dea675b83d4ca
philipotieno <tmitchellb007@gmail.com>
2019-01-21 19:41:07 +0300

    [Feature #163225166] Removes unused files

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from flask import Flask,Blueprint
 from flask import Flask, jsonify
 
 from instance.config import app_config
@@ -7,6 +8,7 @@ from app.api.v1.views.meetupviews import v1_meetups
 from app.api.v2.views.meetupviews import v2_meetups
 from app.api.v1.views.questionsviews import v1_questions
 from app.api.v2.views.questionsviews import v2_questions
+from flask_jwt_extended import (JWTManager)
 
 def create_app(env_name):
 	""" Cretae app """
@@ -15,6 +17,9 @@ def create_app(env_name):
 
 	app = Flask(__name__)
 	app.config.from_object(app_config[env_name])
+
+	jwt = JWTManager(app)
+	
 	app.register_blueprint(v1_user, url_prefix='/api/v1/auth')
 	app.register_blueprint(v2_user, url_prefix='/api/v2/auth')
 	app.register_blueprint(v1_meetups, url_prefix='/api/v1/meetups')
