d081d03cf913f906b7da60a9e25b6211134cf864
philipotieno <tmitchellb007@gmail.com>
2019-01-23 06:16:21 +0300

    [Feature #163391045] Adds method to view posted questions

--- app/api/v2/views/questionsviews.py
+++ app/api/v2/views/questionsviews.py
@@ -14,27 +14,37 @@ db = Database()
 cur = db.cur
 
 @v2_questions.route('', methods=['POST'])
-@jwt_required
-def create_question(current_user):
+# @jwt_required
+def create_question():
     data = request.get_json()
-    title= data['title']
-    body= data['body']
- 
+    # title= data['title']
+    # body= data['body']
+    # user_id= data['user_id']
+    # meetup_id= data['meetup_id']
+
+    current_user = get_jwt_identity()
+    if not data or not data["title"] or not data["body"] or not data["user_id"] or not data["meetup_id"]:
+            return jsonify({'message': 'All fields are required!'}), 400
+    
     query = "SELECT meetup_id from meetups;"
     cur.execute(query)
     meetups = cur.fetchall()
     questions_data = Question(
-    	current_user["id"],
-    	meetup_id["id"],
-    	title,
-    	body
+        data['title'],
+        data['body'],
+        data['user_id'],
+        data['meetup_id']
     	)
-    for m in meetups:
-    	for topic in data['topic'].keys():
-    		if meetups['topic'] == topic:
-    			questions_data.ask_question()
-    			return jsonify({'message': 'Question successfully posted!'}), 201
-    		return jsonify({'message': '{} not available!'.format(meetups)}), 404
+    # for m in meetups:
+    # 	for topic in data['topic'].keys():
+    # 		if meetups['topic'] == topic:
+    # 			questions_data.ask_question()
+    # 			return jsonify({'message': 'Question successfully posted!'}), 201
+    # 		return jsonify({'message': '{} not available!'.format(meetups)}), 404
+    if questions_data.ask_question():
+        return jsonify({'message': 'Question created!'}), 201
+    return jsonify({'message': 'Question {} already exists'.format(data['title'])}), 409
+    
 
 @v2_questions.route('', methods=['GET'])
 def view_all_question():
