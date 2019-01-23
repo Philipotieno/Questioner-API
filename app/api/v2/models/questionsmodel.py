f2d24d10d72e26609f5419666bc108fd6bcf64af
philipotieno <tmitchellb007@gmail.com>
2019-01-23 06:15:16 +0300

    [Start #163391045] Adds questions models

--- app/api/v2/models/questionsmodel.py
+++ app/api/v2/models/questionsmodel.py
@@ -3,28 +3,39 @@ import datetime
 from app.api.v2.models.db import Database
 from app.api.v2.models.meetupsmodel import Meetup
 
+now = datetime.datetime.now()
 db = Database()
-cur = db.conn.cursor()
+cur = db.cur
 
 class Question():
 
     """ Implements questions class"""
 
-    def __init__ (self, user_id, meetup_id, title, body):
-        self.user_id = user_id
-        self.meetup_id = meetup_id
+    def __init__ (self,title, body, user_id, meetup_id):
         self.title = title
         self.body = body
+        self.user_id = user_id
+        self.meetup_id = meetup_id
+        self.created_on = now
 
-    @staticmethod
+    def check_if_question_exists(self, title):
+        query = "SELECT title from questions WHERE title=%s;"
+        cur.execute(query, (title,))
+        question = cur.fetchone()
+        if question:
+            return True
+
     def ask_question(self):
-        query = "INSERT INTO questions (user_id, meetup_id, title, body) values (%s, %s, %s, %s);"
+        if self.check_if_question_exists(self.title):
+            return False
+        query = "INSERT INTO questions (title, body, user_id, meetup_id, created_on) values (%s, %s, %s, %s, %s);"
         cur.execute(
             query,
-            (self.user_id,
+            (self.title,
+             self.body,
+             self.user_id,
              self.meetup_id,
-             self.title,
-             self.body,))
+             self.created_on))
         db.conn.commit()
         return True
 
