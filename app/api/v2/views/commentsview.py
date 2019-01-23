658813b398b81a8b315e1b045a3ebc7361cac15a
philipotieno <tmitchellb007@gmail.com>
2019-01-23 21:44:22 +0300

    [Finishes #163431980] Adds function to view all comments

--- app/api/v2/views/commentsview.py
+++ app/api/v2/views/commentsview.py
@@ -15,21 +15,30 @@ cur = db.cur
 @jwt_required
 def post_comments():
     '''function to post comments'''
-    try:
-	    data = request.get_json()
-	    if not data:
-	        return jsonify({'message': 'No json data entered!'}), 401
+    # try:
+    data = request.get_json()
+    if not data:
+        return jsonify({'message': 'No json data entered!'}), 401
 
-	    if not data or not data["body"] or not data["user_id"] or not data["question_id"]:
-	        return jsonify({'message': 'All fields are required!'}), 400
-	    comments_details = Comment(
-	        data['body'],
-	        data['user_id'],
-	        data['question_id'],
-	    )
+    if not data or not data["body"] or not data["user_id"] or not data["question_id"]:
+        return jsonify({'message': 'All fields are required!'}), 400
+    comments_details = Comment(
+        data['body'],
+        data['user_id'],
+        data['question_id'],
+    )
 
-	    new_comment = comments_details.post_comment()
-	    return jsonify({'message': 'Comment posted successfully!', "data" : new_comment}), 201
+    new_comment = comments_details.post_comment()
+    return jsonify({'message': 'Comment posted successfully!', "data" : new_comment}), 201
 
-    except Exception as e:
-        return jsonify({'message': 'Body cannot be empty enter valid data'}), 400
+    # except Exception as e:
+    #     return jsonify({'message': 'Body cannot be empty enter valid data'}), 400
+
+
+@v2_comments.route('', methods=['GET'])
+def view_all_comments():
+	'''function to get all comments'''
+	comment = Comment.get_all_comments()
+	if comment:
+		return jsonify({'Comment': comment}), 200
+	return jsonify({'message': 'No comments available!'}), 404
