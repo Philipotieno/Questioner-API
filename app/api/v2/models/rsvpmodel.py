8ea91324d4e8a63db2466f617cccc94b035ef214
philipotieno <tmitchellb007@gmail.com>
2019-01-24 06:22:37 +0300

    [Feature 163433492] Adds values to be returned

--- app/api/v2/models/rsvpmodel.py
+++ app/api/v2/models/rsvpmodel.py
@@ -1,5 +1,7 @@
 from app.api.v2.models.db import Database
+import datetime
 
+now = datetime.datetime.now()
 db = Database()
 cur = db.cur
 
@@ -10,28 +12,19 @@ class Rsvp():
     def __init__ (self,user_id, meetup_id, response):
         self.user_id = user_id
         self.meetup_id = meetup_id
-        self.response = response 
+        self.response = response
+        self.created_on = now
 
-    def check_if_rsvp_exists(self, user_id, meetup_id):
-        '''Method to check for existing comments'''
-        query = "SELECT * FROM rsvps WHERE user_id = '{}' AND meetup_id = '{}'\
-        ".format(user_id, meetup_id)
-        cur.execute(query)
-        the_rsvp = cur.fetchone()
-        if the_rsvp:
-            return True
 
     def post_rsvp(self):
-        '''Method to post for comments'''
-        if self.check_if_rsvp_exists(self.user_id, self.meetup_id):
-            return False
-        query = "INSERT INTO rsvps (user_id, meetup_id, response) values (%s, %s, %s) \
-        RETURNING rsvp_id user_id, meetup_id, response;"
+        query = "INSERT INTO rsvps (user_id, meetup_id, response, created_on) values (%s, %s, %s, %s) \
+        RETURNING rsvp_id, user_id, meetup_id, response;"
         cur.execute(
             query,
             (self.user_id,
              self.meetup_id,
-             self.response))
+             self.response,
+             self.created_on))
         rsvp = cur.fetchone()
         db.conn.commit()
         return rsvp
