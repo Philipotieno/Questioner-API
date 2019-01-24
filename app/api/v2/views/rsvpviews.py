3a9c601e20d635b866fe67070e0f7ce2b44f927d
philipotieno <tmitchellb007@gmail.com>
2019-01-24 06:23:06 +0300

    [Feature 163433492] Checks for rsvp response

--- app/api/v2/views/rsvpviews.py
+++ app/api/v2/views/rsvpviews.py
@@ -14,22 +14,30 @@ cur = db.cur
 # @jwt_required
 def post_rsvp(meetup_id):
     """Function to view rsvp """
-    # try:
-    data = request.get_json()
-    if not data:
-        return jsonify({'message': 'No json data entered!'}), 401
+    try:
+        data = request.get_json()
+        if not data:
+            return jsonify({'message': 'No json data entered!'}), 401
 
-    if not data or not data["user_id"] or not data["meetup_id"] or not data["response"]:
-        return jsonify({'message': 'All fields are required!'}), 400
-    rsvp_details = Rsvp(
-        data['user_id'],
-        data['meetup_id'],
-        data['response'],
-    )
+        if not data or not data["user_id"] or not data["meetup_id"] or not data["response"]:
+            return jsonify({'message': 'All fields are required!'}), 400
 
-    new_rsvp = rsvp_details.post_rsvp()
-    return jsonify({'message': 'Rsvp posted successfully!', "data" : new_rsvp}), 201
+        query = "SELECT user_id, meetup_id FROM rsvps;"
+        cur.execute(query)
+        ids = cur.fetchall()
+        for ids in ids:
+            if ids['user_id'] == data['user_id'] and ids['meetup_id'] == data['meetup_id']:
+                return jsonify({'message': 'user already responded to that meetup'}), 409
+
+        rsvp_details = Rsvp(
+            data['user_id'],
+            data['meetup_id'],
+            data['response'],
+        )
+
+        new_rsvp = rsvp_details.post_rsvp()
+        return jsonify({'message': 'Rsvp posted successfully!', "data" : new_rsvp}), 201
 
     except Exception as e:
-        return jsonify({'message': 'Body cannot be empty enter valid data'}), 400
+        return jsonify({'message': str(e)}), 400
     #     return jsonify({'message': 'Body cannot be empty enter valid data'}), 400
