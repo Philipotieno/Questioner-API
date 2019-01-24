7677cb20796c16ceb008c92e208c5120b277078d
philipotieno <tmitchellb007@gmail.com>
2019-01-24 05:29:06 +0300

    [Feature 163433492] Adds function to view rsvps

---
+++ app/api/v2/views/rsvpviews.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, jsonify, request
+
+from app.api.v2.models.rsvpmodel import Rsvp
+from flask_jwt_extended import (jwt_required, get_jwt_identity)
+
+from app.api.v2.models.db import Database
+
+v2_rsvps = Blueprint('v2_rsvps', __name__)
+
+db = Database()
+cur = db.cur
+
+@v2_rsvps.route('', methods=['POST'])
+# @jwt_required
+def post_rsvp(meetup_id):
+    """Function to view rsvp """
+    # try:
+    data = request.get_json()
+    if not data:
+        return jsonify({'message': 'No json data entered!'}), 401
+
+    if not data or not data["user_id"] or not data["meetup_id"] or not data["response"]:
+        return jsonify({'message': 'All fields are required!'}), 400
+    rsvp_details = Rsvp(
+        data['user_id'],
+        data['meetup_id'],
+        data['response'],
+    )
+
+    new_rsvp = rsvp_details.post_rsvp()
+    return jsonify({'message': 'Rsvp posted successfully!', "data" : new_rsvp}), 201
+
+    except Exception as e:
+        return jsonify({'message': 'Body cannot be empty enter valid data'}), 400
+    #     return jsonify({'message': 'Body cannot be empty enter valid data'}), 400
