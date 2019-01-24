ffae104840c735d1984a5e2708508381e4624cd3
philipotieno <tmitchellb007@gmail.com>
2019-01-25 00:55:41 +0300

    [bg #163461937]Fix failing test for meetup

--- app/tests/v2/test_v2_meetups.py
+++ app/tests/v2/test_v2_meetups.py
@@ -1,5 +1,6 @@
 import unittest
 import json
+from flask_jwt_extended import (create_access_token)
 
 from app.tests.v2.base_v2 import TestSetup
 
@@ -7,83 +8,20 @@ class TestMeetup(TestSetup):
 
     def test_created_meetup(self):
         """ Test new meetup can be created """
-        res = self.client.post(
-            '/api/v1/meetups',
-            data=json.dumps(self.meetup_2),
-            content_type='application/json')
-        self.assertEqual(res.status_code, 201)
-        msg = json.loads(res.data.decode("UTF-8"))
-        self.assertIn("Meetup created successfully", msg["Message"])
+        token = create_access_token(identity="wiseadmin")
 
-    def test_unvalidated_meetup(self):
-        """ Test new meetup can be created """
         res = self.client.post(
-            '/api/v1/meetups',
+            '/api/v2/meetups',
+            data=json.dumps(self.meetup_3),
+            content_type='application/json',
+            headers={'Authorization': 'Bearer {}'.format(token)})
+        self.assertEqual(res.status_code, 201)
+
+    def test_meetup_with_no_auth(self):
+        """ Test new meetup with no authorisation"""
+        
+        res = self.client.post(
+            '/api/v2/meetups',
             data=json.dumps(self.meetup_3),
             content_type='application/json')
-        self.assertEqual(res.status_code, 400)
-        msg = json.loads(res.data.decode("UTF-8"))
-        self.assertIn("length of topic, location and tag should not be less than 4", msg["message"])
-
-    def test_get_all_meetup(self):
-
-        self.client.post(
-            '/api/v1/meetups',
-            data=json.dumps(self.add_meetup),
-            content_type='application/json')
-
-        res = self.client.get(
-            '/api/v1/meetups/upcoming',
-            content_type='application/json')
-        self.assertEqual(res.status_code, 200)
-
-    def test_get_specific_meetup(self):
-
-        res = self.client.post(
-            '/api/v1/meetups',
-            data=json.dumps(self.add_meetup),
-            content_type='application/json')
-
-        res = self.client.get(
-            '/api/v1/meetups/1',
-            content_type='application/json')
-        self.assertEqual(res.status_code, 200)
-
-    def test_non_exixting_meetup(self):
-
-        res = self.client.get(
-            '/api/v1/meetups/12',
-            data=json.dumps(self.meetup_1),
-            content_type='application/json')
-        self.assertEqual(res.status_code, 404)
-        msg = json.loads(res.data.decode("UTF-8"))
-        self.assertIn("meetup does not exist", msg["message"])
-
-
-    def test_rsvp_meetup(self):
-
-        self.client.post(
-            '/api/v1/meetups/1',
-            data=json.dumps(self.add_meetup),
-            content_type='application/json')
-
-        res = self.client.post(
-            '/api/v1/meetups/1/rsvp',
-            data=json.dumps(dict(attending="maybe")),
-            content_type='application/json')
-        self.assertEqual(res.status_code, 200)
-        msg = json.loads(res.data.decode("UTF-8"))
-        self.assertIn("your response has been recorded", msg["message"])
-
-    def test_rsvp_non_existent_status(self):
-
-        res = self.client.post(
-            '/api/v1/meetups/44/rsvp',
-            data=json.dumps(dict(attending="nono")),
-            content_type='application/json')
-        self.assertEqual(res.status_code, 404)
-        msg = json.loads(res.data.decode("UTF-8"))
-        self.assertIn("meetup does not exist", msg["message"])
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(res.status_code, 401)
