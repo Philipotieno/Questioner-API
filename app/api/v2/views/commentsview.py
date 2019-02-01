613d7df048bf6d49c3e7c8d6442267adf07f7334
philipotieno <tmitchellb007@gmail.com>
2019-02-01 08:36:01 +0300

    [Bug #163578214]Adds comment id to be returned

--- app/api/v2/views/commentsview.py
+++ app/api/v2/views/commentsview.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
 
 from app.api.v2.models.commentsmodel import Comment
+from app.api.v2.models.questionsmodel import Question
+from app.api.v2.models.usersmodels import User
 from flask_jwt_extended import (jwt_required, get_jwt_identity)
 
 from app.api.v2.models.db import Database
@@ -13,36 +15,37 @@ cur = db.cur
 
 @v2_comments.route('', methods=['POST'])
 @jwt_required
-def post_comments():
+def post_comments(question_id):
     '''function to post comments'''
-    try:
-        data = request.get_json()
-        if not data:
-            return jsonify({'message': 'No json data entered!'}), 401
+    qns = Question.get_specific_question(question_id)
+    if not qns:
+        return jsonify({'message': 'question not found!'}), 404
 
-        if not data or not data["body"] or not data["user_id"] or not data["question_id"]:
-            return jsonify({'message': 'All fields are required!'}), 400
-        try:
-            comments_details = Comment(
-                data['body'],
-                data['user_id'],
-                data['question_id'],
-            )
+    username = get_jwt_identity()
+    user = User.get_user_by_name(username)
 
-            new_comment = comments_details.post_comment()
-            if new_comment:
-                return jsonify({'status':201,'message': 'Comment posted successfully!', "data" : new_comment}), 201
-            return jsonify({'status':409,'message': 'Comment already exists!'}), 409
-        except Exception:
-            return jsonify({'status':400, 'error': 'User_id or question id does not exist!'}), 400
-    except Exception as e:
-        return jsonify({'status':400,'error': 'Body cannot be empty enter valid json data'}), 400
+    data = request.get_json()
+    if not data:
+        return jsonify({'message': 'No json data entered!'}), 401
 
+    if not data or not data["body"] or " ":
+        return jsonify({'message': 'All fields are required!'}), 400
 
-@v2_comments.route('', methods=['GET'])
-def view_all_comments():
-	'''function to get all comments'''
-	comment = Comment.get_all_comments()
-	if comment:
-		return jsonify({'status':200,'Comment': comment}), 200
-	return jsonify({'message': 'No comments available!'}), 404
+    query = "SELECT user_id, question_id, body FROM comments;"
+    cur.execute(query)
+    cmts = cur.fetchall()
+    for cmts in cmts:
+        if cmts['user_id'] == user['user_id'] and cmts['question_id'] == qns['question_id'] \
+        and cmts['body'] == data['body']:
+            return jsonify({'message': 'Such a comment already exist for that question'}), 409
+
+    comments_details = Comment(
+        data['body'],
+        user['user_id'],
+        qns['question_id']
+    )
+
+    new_comment = comments_details.post_comment()
+    if new_comment:
+        return jsonify({'status':201,'message': 'Comment posted successfully!', "data" : new_comment}), 201
+    return jsonify({'status':409,'message': 'Comment already exist for that question!'}), 409
