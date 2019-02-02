5d78c01fa513acbabfdd2e6cdd571b98007451c5
philipotieno <tmitchellb007@gmail.com>
2019-02-02 09:45:37 +0300

    Adds docstring

--- app/api/v2/models/usersmodels.py
+++ app/api/v2/models/usersmodels.py
@@ -1,14 +1,15 @@
-import os
+'''User models'''
 from app.api.v2.models.db import Database
 
 db = Database()
 cur = db.cur
 
-class User(object):
+class User:
 
     """ Implements user class"""
 
     def __init__(self, firstname, lastname, username, phone_number, email, password, admin=False):
+        '''initializez the class'''
         self.firstname = firstname
         self.lastname = lastname
         self.username = username
@@ -16,8 +17,8 @@ class User(object):
         self.phone_number = phone_number
         self.password = password
         self.admin = admin
-        
     def check_if_user_exists(self, username):
+        '''Method to checks if username exists'''
         query = "SELECT username from users WHERE username=%s;"
         cur.execute(query, (username,))
         the_user = cur.fetchone()
@@ -25,9 +26,12 @@ class User(object):
             return True
 
     def register_user(self):
+        '''Method to register a new user'''
         if self.check_if_user_exists(self.username):
             return False
-        query = "INSERT INTO users (firstname, lastname, username, phone_number, email, password, admin) VALUES (%s, %s, %s, %s, %s, %s, %s) \
+        query = "INSERT INTO users \
+        (firstname, lastname, username, phone_number, email, password, admin) \
+        VALUES (%s, %s, %s, %s, %s, %s, %s) \
         RETURNING user_id, firstname, lastname, username, phone_number, email;"
         cur.execute(
             query,
@@ -41,9 +45,9 @@ class User(object):
         user = cur.fetchone()
         db.conn.commit()
         return user
-        
     @staticmethod
     def get_user_by_name(username):
+        '''Method to get user by username'''
         query = "SELECT * FROM users WHERE username=%s;"
         cur.execute(query, (username, ))
         user = cur.fetchone()
